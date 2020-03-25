4e57dc30c0856e3e49f27d142782137014701e19
Juliet George <georgejuliet11@gmail.com>
2020-03-24 23:56:56 -0700

    first draft

---
+++ script.js
@@ -0,0 +1,90 @@
+var timeEL = 
+
+/*
+timer pseudocode:
+1)click start button and the timer starts
+
+2) if answer is wrong - 5 seconds get removed from the timeleft
+
+3) when timer reaches, the game is over.
+
+*/ 
+var totalScore = 0;
+var timeDisplay = 100;
+var timeRemaining; 
+
+
+
+/*
+main.html
+
+1) has the timer and the highscore links at the top right and left
+link to view highscore is on the left // time is on the right 
+2) has the start button 
+3) reflects question when timer starts
+4) question has buttons 
+5)shows a form for user to add initial
+6) saves and returns initial of user and final score
+
+
+highscore
+view high score link 
+1) stores the highest score for the user. 
+
+
+
+
+
+*/
+
+var questions = [
+{   question:'Critical thinking concerns…',
+    options:['Determining the cause of our beliefs', 'Pinpointing the psychological basis of our beliefs','Determining the quality of our beliefs', 'Assessing the practical impact of our beliefs' ],
+    answer:'Determining the quality of our beliefs.'
+
+},
+
+{   question:'A belief is worth accepting if',
+    options:['We have good reasons to accept it','It is consistent with our needs','It has not been proven wrong','It is accepted by our peers'],
+    answer:'We have good reasons to accept it.'
+
+},
+
+{   question:'The word critical in critical thinking refers to…',
+    options:['A fault-finding attitude','Attempts to win an argument','Using careful judgment or judicious evaluation',' A lack of respect for other people'],
+    answer:'Using careful judgment or judicious evaluation.'
+
+},
+
+{   question:'A statement is…',
+    options:['A question or exclamation', 'An affirmation of prior beliefs', 'An assertion that something is or is not the case', 'An assertion that is neither true nor false'],
+    answer:'An assertion that something is or is not the case.'
+
+},
+
+{   question:'According to the text, critical thinking complements…',
+    options:['Our prejudices', 'Our emotions', 'Peer pressure', 'Our unconscious desires'],
+    answer:'Our emotions.'
+
+},
+
+
+{   question:'Statements backed by good reasons are…',
+    options:['Worthy of strong acceptance','To be believed with certainty','Never false','Beyond all possible doubt'],
+    answer:'Worthy of strong acceptance.'
+
+}
+
+]
+
+
+
+
+
+
+
+
+
+
+
+]
