65aaec6b43688c84cc7335edcfd2eaa3e8cc0d49
Juliet George <georgejuliet11@gmail.com>
2020-03-26 01:53:27 -0700

    organizing

--- questions.js
+++ questions.js
@@ -1,35 +1,35 @@
 var questions = [
     {
         question: 'Critical thinking concerns…',
-        options: ['Determining the cause of our beliefs', 'Pinpointing the psychological basis of our beliefs', 'Determining the quality of our beliefs', 'Assessing the practical impact of our beliefs'],
+        options: ['Determining the cause of our beliefs.', 'Pinpointing the psychological basis of our beliefs.', 'Determining the quality of our beliefs.', 'Assessing the practical impact of our beliefs.'],
         answer: 'Determining the quality of our beliefs.'
 
     },
 
     {
         question: 'A belief is worth accepting if',
-        options: ['We have good reasons to accept it', 'It is consistent with our needs', 'It has not been proven wrong', 'It is accepted by our peers'],
+        options: ['We have good reasons to accept it.', 'It is consistent with our needs.', 'It has not been proven wrong.', 'It is accepted by our peers.'],
         answer: 'We have good reasons to accept it.'
 
     },
 
     {
         question: 'The word critical in critical thinking refers to…',
-        options: ['A fault-finding attitude', 'Attempts to win an argument', 'Using careful judgment or judicious evaluation', ' A lack of respect for other people'],
+        options: ['A fault-finding attitude.', 'Attempts to win an argument.', 'Using careful judgment or judicious evaluation.', ' A lack of respect for other people.'],
         answer: 'Using careful judgment or judicious evaluation.'
 
     },
 
     {
         question: 'A statement is…',
-        options: ['A question or exclamation', 'An affirmation of prior beliefs', 'An assertion that something is or is not the case', 'An assertion that is neither true nor false'],
+        options: ['A question or exclamation.', 'An affirmation of prior beliefs.', 'An assertion that something is or is not the case.', 'An assertion that is neither true nor false.'],
         answer: 'An assertion that something is or is not the case.'
 
     },
 
     {
         question: 'According to the text, critical thinking complements…',
-        options: ['Our prejudices', 'Our emotions', 'Peer pressure', 'Our unconscious desires'],
+        options: ['Our prejudices.', 'Our emotions.', 'Peer pressure.', 'Our unconscious desires.'],
         answer: 'Our emotions.'
 
     },
@@ -37,7 +37,7 @@ var questions = [
 
     {
         question: 'Statements backed by good reasons are…',
-        options: ['Worthy of strong acceptance', 'To be believed with certainty', 'Never false', 'Beyond all possible doubt'],
+        options: ['Worthy of strong acceptance.', 'To be believed with certainty.', 'Never false.', 'Beyond all possible doubt.'],
         answer: 'Worthy of strong acceptance.'
 
     }
--- script.js
+++ script.js
@@ -15,9 +15,9 @@ var inputInitial = document.getElementById("inputInitial")
 var correctAnswer;
 var optionsAnswer;
 var totalScore = 0;
-var timeDisplay = 100;
+var timeDisplay = 20;
 var userInitial;
-var timeRemaining; 
+var timeRemaining;
 var questionArray = -1 // this will help retrieve values from the array
 //var userScore;
 var highScore;
@@ -26,124 +26,232 @@ var highScore;
 
 startEL.addEventListener("click", startTime);
 
-submitBtnEL.addEventListener("click", function(event){
-event.stopPropagation();
-finalScore();
-//console.log("test")
+submitBtnEL.addEventListener("click", function (event) {
+    event.stopPropagation();
+    finalScore();
+    //console.log("test")
 
-window.location.href = "scores.html"
+    window.location.href = "scores.html"
 
 
 });
 
-for (var i = 0; i < optionBtn.length; i++) {
-    optionBtn[i].addEventListener("click", function userChoice(event) {
-        event.stopPropagation();
 
-        if (event.currentTarget.innerText === correctAnswer) {
-            totalScore++
-            alertUser.innerHTML = "Correct!";
-            setTimeout(removeAlert,1000)
-            showAlert(); //create this
-        } else {
-            totalScore--
-            alertUser.innerHTML = "Please review course material!";
-            setTimeout(removeAlert,3000)
-            timeDisplay = timeDisplay - 20;
-            showAlert();
-        }
-        questionArray++
-        if(questionArray < 6){
-            displayQuestions;
-        }
+optionsEL.addEventListener("click", function (event) {
+    event.stopPropagation();
 
-    });
-}
-
-
-
-
-//timer
-function setTime() {
-    var timerIn = setInterval(function () {
-        timeDisplay--;
-        timeEL.innerHTML = "Time remaining: " + timeDisplay;
-
-
-    if(timeDisplay === 0 || questions.length === questionArray){
-        clearInterval(timerIn);
-        alert("Quiz Over");
-        setTimeout(quizOver,1000); //need to figure this out
+    if (event.target.textContent === correctAnswer) {
+        totalScore++
+        alertUser.innerHTML = "Correct!";
+        setTimeout(removeAlert, 1000)
+        showAlert(); //create this
+    } else {
+        totalScore--
+        alertUser.innerHTML = "Please review course material!";
+        setTimeout(removeAlert, 1000)
+        timeDisplay = timeDisplay - 1;
+        showAlert();
     }
-    },1000)
-
-}
-
-function quizOver(){
-    questionScreen.classList.add('d-none');
-    submitScoreEL.classList.remove('d-none');
-    document.getElementById("user-score").textContent = "Your final score is " + totalScore + "!"
- 
-}
-
-function finalScore() {
-    userInitial = inputInitial.Value
-    var userScore = {
-        initial: userInitial,
-        score: totalScore
-    }
-    highScore = JSON.parse(localStorage.getItem('highScore'))
-    highScore.push(userScore)
-    localStorage.setItem("highScore", JSON.stringify(highScore))
-    // convert into string and add into array and add to local storage. 
-}
-
-
-
-
-function storeScore(){
-
-}
-//we start the timer by clicking the button
-function startTime(){
-
-    // after we click the button,
-    //we have to remove the first screen and put the second using the classlist.add and classlist.remove
-    firstScreen.classList.add('d-none')
-    questionScreen.classList.remove('d-none')
-    setTime();
     displayQuestions();
-}
 
-function displayQuestions(){
-    questionArray++;
-    questionsEL.textContent = questions[questionArray].question;
-    correctAnswer = questions[questionArray].answer;
-    optionsAnswer = questions[questionArray].options;
-    for(var i = 0; i < optionsAnswer.length; i++){
-        optionBtn.textContent = optionsAnswer[i]
-    }    
+})
+
+/*
+$("#options").on("click", function(){
+    var temp = $(this).text();
+    console.log(temp) 
+    console.log(correctAnswer)
+    if (temp === correctAnswer) {
+        totalScore++
+        $(".alert").text("Correct!");
+        setTimeout(removeAlert, 1000)
+        showAlert(); //create this
+    } else {
+        totalScore--
+        $(".alert").text("Please review course material!");
+        setTimeout(removeAlert, 1000)
+        timeDisplay = timeDisplay - 20;
+        showAlert();
+    }
+    displayQuestions();
     
-}    
-    
-function removeAlert() {
-    alertUser.style.display = "none"; //set alert user to not be displayed
+})
 
 
-}
 
-function showAlert() {
-    alertUser.removeAttribute('style');
-    
-    
+$(".option1").on("click", function () {
+    var temp = $(this).text();
+    console.log(temp)
+    console.log(correctAnswer)
+    if (temp === correctAnswer) {
+        totalScore++
+        $(".alert").text("Correct!");
+        setTimeout(removeAlert, 1000)
+        showAlert();
+    } else {
+        totalScore--
+        $(".alert").text("Please review course material!");
+        setTimeout(removeAlert, 1000)
+        timeDisplay = timeDisplay - 20;
+        showAlert();
+    }console.log(totalScore)
+    displayQuestions();
+})
+
+$(".option2").on("click", function () {
+    var temp = $(this).text();
+    if (temp === correctAnswer) {
+        totalScore++
+        $(".alert").text("Correct!");
+        setTimeout(removeAlert, 1000)
+        showAlert(); //create this
+    } else {
+        totalScore--
+        $(".alert").text("Please review course material!");
+        setTimeout(removeAlert, 1000)
+        timeDisplay = timeDisplay - 20;
+        showAlert();
+    }
+    displayQuestions();
+})
+
+
+$(".option3").on("click", function () {
+    var temp = $(this).text();
+    if (temp === correctAnswer) {
+        totalScore++
+        $(".alert").text("Correct!");
+        setTimeout(removeAlert, 1000)
+        showAlert(); //create this
+    } else {
+        totalScore--
+        $(".alert").text("Please review course material!");
+        setTimeout(removeAlert, 1000)
+        timeDisplay = timeDisplay - 20;
+        showAlert();
+    }
+    displayQuestions();
+})
+
+$(".option4").on("click", function () {
+    var temp = $(this).text();
+    if (temp === correctAnswer) {
+        totalScore++
+        $(".alert").text("Correct!");
+        setTimeout(removeAlert, 1000)
+        showAlert(); //create this
+    } else {
+        totalScore--
+        $(".alert").text("Please review course material!");
+        setTimeout(removeAlert, 1000)
+        timeDisplay = timeDisplay - 20;
+        showAlert();
+    }
+    displayQuestions();
+})
+*/
+
+
+
+    //timer
+    function setTime() {
+        var timerIn = setInterval(function () {
+            timeDisplay--;
+            timeEL.innerHTML = "Time remaining: " + timeDisplay;
+
+
+            if (timeDisplay === 0 || questions.length === questionArray) {
+                clearInterval(timerIn);
+                alert("Quiz Over");
+                setTimeout(quizOver, 500); //need to figure this out
+            }
+        }, 1000);
+
+    }
+
+    function quizOver() {
+        questionScreen.classList.add('d-none');
+        submitScoreEL.classList.remove('d-none');
+        document.getElementById("user-score").textContent = "Your final score is " + totalScore + "!"
+
+    }
+
+    function finalScore() {
+        userInitial = inputInitial.Value
+        var userScore = {
+            initial: userInitial,
+            score: totalScore
+        }
+        highScore = JSON.parse(localStorage.getItem('highScore'))
+        highScore.push(userScore)
+        localStorage.setItem("highScore", JSON.stringify(highScore))
+        // convert into string and add into array and add to local storage. 
     }
 
 
-    
+    //we start the timer by clicking the button
+    function startTime() {
 
-  
+        // after we click the button,
+        //we have to remove the first screen and put the second using the classlist.add and classlist.remove
+        firstScreen.classList.add('d-none')
+        questionScreen.classList.remove('d-none')
+        setTime();
+        displayQuestions();
+    }
 
-    
+    function displayQuestions() {
+        questionArray++;
+        //optionsEL.textContent = "";
+        questionsEL.textContent = questions[questionArray].question;
+        correctAnswer = questions[questionArray].answer;
+        //console.log(correctAnswer)
+        optionsAnswer = questions[questionArray].options;
+        for (var i = 0; i < optionsAnswer.length; i++) {
+            //optionsEL.innerHTML = optionsAnswer[i]
+            //var li = document.createElement('li')
+            //li.textContent = optionsAnswer[i]
+            //optionBtn.appendChild(optionsEL)
+            //console.log(optionsAnswer[0])
+            var option1 = document.querySelector(".option1")
+            option1.innerHTML = optionsAnswer[0]
+
+            var option2 = document.querySelector(".option2")
+            option2.innerHTML = optionsAnswer[1]
+
+            var option3 = document.querySelector(".option3")
+            option3.innerHTML = optionsAnswer[2]
+
+            var option4 = document.querySelector(".option4")
+            option4.innerHTML = optionsAnswer[3]
+
+
+
+        }
+
+
+
+
+    }
+
+    function removeAlert() {
+        alertUser.style.display = "none"; //set alert user to not be displayed
+
+
+    }
+
+    function showAlert() {
+        alertUser.removeAttribute('style');
+
+
+    }
+
+
+
+
+
+
+
 
 
 
@@ -190,28 +298,27 @@ timer pseudocode:
 
 3) when timer reaches 0, the game is over.
 
-*/ 
+*/
 
 /*
 main.html
 
 1) has the timer and the highscore links at the top right and left
-link to view highscore is on the left // time is on the right 
-2) has the start button 
+link to view highscore is on the left // time is on the right
+2) has the start button
 3) reflects question when timer starts
-4) question has buttons 
+4) question has buttons
 5)shows a form for user to add initial
 6) saves and returns initial of user and final score
 
 
 highscore
-view high score link 
-1) stores the highest score for the user. 
+view high score link
+1) stores the highest score for the user.
 
 
 
 
 
 */
-
 
